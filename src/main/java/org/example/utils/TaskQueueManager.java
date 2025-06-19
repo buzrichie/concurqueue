62e75a55591c5c696eae1b3e4ab687e1f71a29f0
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-19 03:44:22 -0700

    feat(util): add TaskQueueManager to centalise shared field data

---
+++ src/main/java/org/example/utils/TaskQueueManager.java
@@ -0,0 +1,15 @@
+package org.example.utils;
+
+import org.example.model.Task;
+import org.example.model.TaskStatus;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TaskQueueManager {
+    public static PriorityBlockingQueue<Task> taskQueue = new PriorityBlockingQueue<Task>();
+    public static ConcurrentHashMap<UUID, TaskStatus> taskStatusMap = new ConcurrentHashMap<java.util.UUID, TaskStatus>();
+    public static AtomicInteger taskProcessedCounter = new AtomicInteger();
+}
