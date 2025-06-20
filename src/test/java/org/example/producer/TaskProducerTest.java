7c1e9af13174ac4ee5419aa0bb9f75df1bfe91f7
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-19 19:33:18 -0700

    test(TaskProducer): test creating of task, possible queue emptyness and more

---
+++ src/test/java/org/example/producer/TaskProducerTest.java
@@ -0,0 +1,34 @@
+package org.example.producer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.example.model.TaskStatus;
+import org.example.utils.TaskQueueManager;
+import org.junit.jupiter.api.*;
+import static org.awaitility.Awaitility.await;
+import java.time.Duration;
+
+public class TaskProducerTest {
+
+    @BeforeEach
+    public void setup() {
+        TaskQueueManager.taskQueue.clear();
+        TaskQueueManager.taskStatusMap.clear();
+    }
+
+    @Test
+    public void testProducerAddsTasksToQueue() {
+        Thread producerThread = new Thread(new TaskProducer("TEST-PRODUCER"));
+        producerThread.start();
+
+        await().atMost(Duration.ofSeconds(5))
+                .until(() -> !TaskQueueManager.taskQueue.isEmpty());
+
+        assertFalse(TaskQueueManager.taskQueue.isEmpty());
+        assertEquals(TaskStatus.SUBMITTED,
+                TaskQueueManager.taskStatusMap
+                        .values().iterator().next());
+
+        producerThread.interrupt();
+    }
+}
