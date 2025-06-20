61a668c7f9c972aa4e629ed2a483ea225ee6a25f
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-19 19:01:19 -0700

    perf(model): add task retryCount field and method to manipulate or increment it

--- src/main/java/org/example/model/Task.java
+++ src/main/java/org/example/model/Task.java
@@ -3,12 +3,14 @@ package org.example.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Data
 @Builder
+@Getter
 @AllArgsConstructor
 public class Task implements Comparable<Task> {
     private final UUID id;
@@ -16,6 +18,12 @@ public class Task implements Comparable<Task> {
     private final int priority;
     private final Instant createdTimestamp;
     private final String payload;
+    private int retryCount;
+
+    public void incrementRetryCount() {
+        this.retryCount++;
+    }
+
 
     @Override
     public int compareTo(Task other) {
