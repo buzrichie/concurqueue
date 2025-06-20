ecf1032ead083e7a9a6eebcad5dd776017eb75a6
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-06-19 19:22:23 -0700

    perf(util:TaskQueueManage): add LinkedBlockingQueue - enforce bound on task queue size

--- src/main/java/org/example/utils/TaskQueueManager.java
+++ src/main/java/org/example/utils/TaskQueueManager.java
@@ -5,11 +5,15 @@ import org.example.model.TaskStatus;
 
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TaskQueueManager {
-    public static PriorityBlockingQueue<Task> taskQueue = new PriorityBlockingQueue<Task>();
+//    public static PriorityBlockingQueue<Task> taskQueue = new PriorityBlockingQueue<Task>();
+    public static final int MAX_CAPACITY = 10;
+    public static final LinkedBlockingQueue<Task> taskQueue = new LinkedBlockingQueue<>(MAX_CAPACITY);
+
     public static ConcurrentHashMap<UUID, TaskStatus> taskStatusMap = new ConcurrentHashMap<java.util.UUID, TaskStatus>();
     public static AtomicInteger taskProcessedCounter = new AtomicInteger();
     public static int primitiveTaskProcessedCounter;
